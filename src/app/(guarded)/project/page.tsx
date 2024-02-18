215e9d03a8cd158f5c238d2f52aaba7d77799dd5
djasnive <djasniveyrajaona@gmail.com>
2024-02-19 00:13:22 +0300

    feat(project):user can list project from the database

--- src/app/(guarded)/project/page.tsx
+++ src/app/(guarded)/project/page.tsx
@@ -2,8 +2,11 @@ import PageTitle from "@/components/pages/PageTitle";
 import { Button } from "@/components/ui/button";
 import { FolderPlus } from "lucide-react";
 import Link from "next/link";
+import db from '@/lib/db';
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
-export default function ProjectPage(){
+export default async function ProjectPage() {
+    const projects = await db.project.findMany();
     return (
         <div>
             <PageTitle action={
@@ -11,8 +14,13 @@ export default function ProjectPage(){
                     <Link href="/project/create" className="btn-primary" ><FolderPlus width={18} /> New</Link>
                 </>
             }>Project</PageTitle>
-            <div className="">
-                
+            <div className="flex flex-col gap-2 py-2">
+                {projects.map(project => (
+                    <Card key={project.id}>
+                        <CardHeader>{project.name}</CardHeader>
+                        {project.description && <CardContent>{project.description}</CardContent>}
+                    </Card>
+                ))}
             </div>
         </div>
     )
