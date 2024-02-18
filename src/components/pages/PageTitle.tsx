cb8071938378f80030490c0466fc2a5c1580cb66
djasnive <djasniveyrajaona@gmail.com>
2024-02-18 19:05:04 +0300

    refractor: definir le titre de la page comme bold

--- src/components/pages/PageTitle.tsx
+++ src/components/pages/PageTitle.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export default function PageTitle({ children }:{children: ReactNode}){
     return (
         <div className="w-full">
-            <div className="text-lg">{children}</div>
+            <div className="text-xl font-semibold">{children}</div>
         </div>
     );
 }
